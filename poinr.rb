ccaac6c10f71c68aa25b5c117fe1f4b3e00daa5b
zh-chichkan <zh.chichkan@gmail.com>
2012-11-14 15:58:47 +0200

    add test and methot dis

--- poinr.rb
+++ poinr.rb
@@ -9,5 +9,9 @@ class Poinr
   def to_s
      "(#@x,#@y)"
   end  
+
+  def dis(other)
+     Math.sqrt((other.x-x)**2+((other.y-y)**2)
+  end
   
 end
--- spec/poinr_spec.rb
+++ spec/poinr_spec.rb
@@ -2,10 +2,13 @@ require 'spec_helper'
 
 describe Poinr do
   let(:p) { Poinr.new(1,1) }
+  let(:a) { Poinr.new(1,2) }
 
    it "should be (x,y) when Poinr is (x,y)" do
       p.to_s.should == "(1,1)"
    end
- 
-
+   it "should be distance between two points" do
+	a.dis(p).should == 1.0
+   end
+   
 end
