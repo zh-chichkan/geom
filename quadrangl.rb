8abba4117962a0233853aca4d8b399eb600bc305
zh-chichkan <zh.chichkan@gmail.com>
2012-11-14 16:27:29 +0200

    add class Quadrangle

--- quadrangl.rb
+++ quadrangl.rb
@@ -1,3 +1,19 @@
 class Quadrangl
+   attr_reader :a, :b, :c, :d
+
+   def initialize(a,b,c,d)
+      @a = a
+      @b = b			
+      @c = c
+      @d = d
+   end  
+
+   def to_s
+      "a#@a b#@b c#@c d#@d"
+   end  
+
+   def perimeter
+      (@a.dis(@b))+(@b.dis(@c))+(@c.dis(@d))+(@d.dis(@a))
+   end
 
 end
--- spec/quadrangl_spec.rb
+++ spec/quadrangl_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-  describe Quadrangl do
+ describe Quadrangl do
      
      let(:a) { Poinr.new(-2,2) }
      let(:b) { Poinr.new(4,2) }
@@ -11,15 +11,11 @@ require 'spec_helper'
      it "should by 'a(x,y), b(x,y) c(x,y) d(x,y)'" do
         q.to_s.should == "a(-2,2) b(4,2) c(4,-2) d(-2,-2)"
      end
-
-     it "should be distance between two points" do
-	a dis c.should == 4.0
-     end
-  
+     
      it "should be perimeter for point: a, b, c, d" do
 	q.perimeter.should == 20.0
      end
 
-  end
+end
 
 
