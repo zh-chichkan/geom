21ff89b11ece082961b3f6999b74b09633f2a21b
zh-chichkan <zh.chichkan@gmail.com>
2012-11-14 14:55:26 +0200

    edited spec poinr_spec

--- spec/poinr_spec.rb
+++ spec/poinr_spec.rb
@@ -1,26 +1,11 @@
 require 'spec_helper'
 
 describe Poinr do
- let(:p) { Poinr.new }
+  let(:p) { Poinr.new(1,1) }
 
- describe "#initialize" do
-   it "should be initialized as emty storage" do
-     Poinr.new.empty?.should be_true 
-   end
- end
-
- describe "#empty?" do
-   it "should be ture when Poinr is empty" do
-     p.stub(:x => [], :y => [])
-     p.empty?.should be_false	   
-   end
- end
-
- describe "#to_s" do
    it "should be (x,y) when Poinr is (x,y)" do
-     p.stub(:x => 1, :y => 1)
-     p.to_s.should == '(1,1)'
+      p.to_s.should == "(1,1)"
    end
- end
+ 
 
 end
