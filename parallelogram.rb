ba66f67b6b19669dd450cf4c6f8a86cc9edd594d
zh-chichkan <zh.chichkan@gmail.com>
2012-11-14 18:38:23 +0200

    add class Parallelogram

--- parallelogram.rb
+++ parallelogram.rb
@@ -1,3 +1,8 @@
-class Parallelogram
+class Parall 
+
+   def plosh
+      h=Poinr.new(b.x,a.y)
+      a.dis(d)*bdis(h)
+   end
 
 end
--- spec/parallelogram_spec.rb
+++ spec/parallelogram_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe Parallelogram do
+describe Parall do
      let(:a) { Poinr.new(-4,-2) }
      let(:b) { Poinr.new(-4,3) }
      let(:c) { Poinr.new(4,-3) }
      let(:d) { Poinr.new(4,-2) }
-     let(:q) { Parallelogram.new(a,b,c,d)}
+     let(:q) { Parall.new(a,b,c,d)}
 
      it "проверка метода площадь"do
         q.plosh.should == 45
